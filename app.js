05ad71659d68ae0bbe9d3b35957f4c731ccc7603
jiramethnate <jiramethneatpatiparn@gmail.com>
2023-08-15 15:20:32 +0700

    Change Port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ModbusRTU = require("modbus-serial");
 const app = express();
-const port = 3000;
+const port = 3012;
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
