2323606dbe2f85c75b77768e9078b3e547964948
jiramethnate <135225542+jiramethnate@users.noreply.github.com>
2023-08-18 14:52:09 +0700

    Multi Send Address

--- app.js
+++ app.js
@@ -16,25 +16,55 @@ app.use(express.json());
 
 // Handle form submission
 app.post("/submit", async (req, res) => {
-  const { ip, port: modbusPort, slaveId, address, value } = req.body;
-  const client = new ModbusRTU();
+  const { ip, port: modbusPort, slaveId, address, value} = req.body;
+  console.log(req.body)
 
   try {
-    	// Open the connection to the FX5 PLC
-	await client.connectTCP(ip, { port: modbusPort});
-	client.setID(slaveId)
-	console.log("connect successfully")
+    const client = new ModbusRTU();
 
-	// Write Holding Register
-	await client.writeRegisters(address, value, function(err, data) {
-		console.log("write register successfully")
-		console.log(data)
-	});
-	
+    let registers = []
+
+    for (let i = 0; i < address.length; i++) {
+      registers.push({
+        address: address[i],
+        value: value[i]
+      })
+    }
+
+    console.log(registers)
+
+    // Open the TCP connection
+    client.connectTCP(ip, { port: modbusPort }, () => {
+
+      writeRegistersSequentially(registers, () => {
+        // Close the connection after writing
+        client.close(() => {
+          console.log("Connection closed.");
+        });
+      });
+    });
+
+    function writeRegistersSequentially(registers, callback) {
+      if (registers.length === 0) {
+        callback();
+        return;
+      }
+
+      const register = registers.shift();
+      client.setID(slaveId); // Set the current slave ID
+      client.writeRegisters(register.address, [register.value], (err) => {
+        if (err) {
+          console.error(`Error writing register ${register.address}:`, err);
+        } else {
+          console.log(`Register ${register.address} written successfully.`);
+        }
+
+        // Continue with the next register
+        writeRegistersSequentially(registers, callback);
+      });
+    }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ success: false, error: error.message });
-  } finally {
-    client.close();
   }
 });
