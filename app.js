ca742f38791ae36cdc6a47dbaa0df0fa3150366e
jiramethnate <135225542+jiramethnate@users.noreply.github.com>
2023-08-21 15:10:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,69 +5,28 @@ const port = 3012;
 
 app.use(express.static("public"));
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post("/submit", async (req, res) => {
   const { ip, port: modbusPort, slaveId, address, value } = req.body;
-  console.log(req.body);
-
+  
   try {
-    let resMessage = [];
-    let registers = [];
+    const client = new ModbusRTU();
+    await client.connectTCP(ip, { port: modbusPort });
+    
+    const resMessage = [];
 
     for (let i = 0; i < address.length; i++) {
-      registers.push({
-        address: address[i],
-        value: value[i],
-      });
+      try {
+        await client.writeRegisters(address[i], [value[i]]);
+        resMessage.push(`Register ${address[i]} written successfully.`);
+      } catch (err) {
+        console.error(`Error writing register ${address[i]}:`, err);
+      }
     }
 
-    console.log(registers);
-
-    function writeRegistersSequentially(ip, modbusPort, slaveId, registers) {
-      return new Promise((resolve, reject) => {
-        const client = new ModbusRTU();
-        client.connectTCP(ip, { port: modbusPort });
-
-        function writeNextRegister() {
-          if (registers.length === 0) {
-            resolve();
-            return;
-          }
-
-          const register = registers.shift();
-          client.setID(slaveId);
-          try {
-            client.writeRegisters(register.address, [register.value]);
-            resMessage.push(
-              `Register ${register.address} written successfully.`
-            );
-          } catch (err) {
-            console.error(`Error writing register ${register.address}:`, err);
-          }
-
-          writeNextRegister();
-        }
-
-        writeNextRegister();
-
-        client.on("close", () => {
-          resolve();
-        });
-
-        client.on("error", (err) => {
-          reject(err);
-        });
-      });
-    }
-
-    await writeRegistersSequentially(ip, modbusPort, slaveId, registers);
+    await client.close();
 
     res.json({ success: true, data: resMessage });
   } catch (error) {
@@ -77,28 +36,22 @@ app.post("/submit", async (req, res) => {
 });
 
 app.post("/submit_read", async (req, res) => {
-  console.log(req.body);
-  const ip = req.body.readIp;
-  const port = req.body.readModbusPort;
-  const slaveId = req.body.readSlaveId;
-  const address = req.body.readAddress;
-  const length = req.body.readLength;
+  const { readIp, readModbusPort, readSlaveId, readAddress, readLength } = req.body;
 
   try {
     const client = new ModbusRTU();
+    await client.connectTCP(readIp, { port: readModbusPort });
 
-    await client.connectTCP(ip, { port: port });
-    console.log("Connected successfully");
-
-    client.readHoldingRegisters(slaveId, address, length, (err, data) => {
+    client.readHoldingRegisters(readSlaveId, readAddress, readLength, (err, data) => {
       if (err) {
         console.error("Error reading holding registers:", err);
+        res.status(500).json({ success: false, error: err.message });
       } else {
         console.log("Holding registers:", data.data);
+        res.json({ success: true, data: data.data });
       }
 
       client.close(() => {
-        res.json({ success: true, data: data });
         console.log("Connection closed");
       });
     });
@@ -106,4 +59,9 @@ app.post("/submit_read", async (req, res) => {
     console.error("Error:", error);
     res.status(500).json({ success: false, error: error.message });
   }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
